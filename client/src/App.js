1a08f34286b078fd23157a4f012e360f3dd8151c
manisha38859 <115543509+manisha38859@users.noreply.github.com>
2024-03-15 16:43:20 +0530

    added files

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import Notifications from './pages/Notifications'
+
+import { Button } from 'antd'
+import { Toaster } from "react-hot-toast";
+import { useSelector } from "react-redux"
+import ProtectedRoute from './components/ProtectedRoute'
+import PublicRoute from './components/PublicRoute'
+import ApplyDoctor from './pages/ApplyDoctor'
+function App() {
+  const {loading} = useSelector(state => state.alerts);
+  return (
+    <BrowserRouter>
+      {loading && (<div className="spinner-parent">
+        <div class="spinner-border" role="status">
+            
+        </div>
+      </div>)}
+      <Toaster position="top-center" reverseOrder={false}/>
+
+      <Routes>
+
+        <Route path='/login' element={<PublicRoute><Login /></PublicRoute>} />
+        <Route path='/register' element={<PublicRoute><Register /></PublicRoute>} />
+        <Route path='/' element={<ProtectedRoute><Home/></ProtectedRoute>} />
+     
+
+       <Route path='/apply-doctor' element={<ProtectedRoute><ApplyDoctor/></ProtectedRoute>} />
+       <Route path='/notifications' element={<ProtectedRoute><Notifications/></ProtectedRoute>} />
+       </Routes>
+      
+
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
