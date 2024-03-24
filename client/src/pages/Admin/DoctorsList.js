1e4d5744a4171415ff08339d4a418f1fb9a53edf
Saloni8318 <salonijnvgzp@gmail.com>
2024-03-24 23:17:05 +0530

    added status change features

--- client/src/pages/Admin/DoctorsList.js
+++ client/src/pages/Admin/DoctorsList.js
@@ -1,39 +1,62 @@
 import React from 'react'
-import {coloumns ,Table} from'antd'
+import { coloumns, Table } from 'antd'
 import { useDispatch } from 'react-redux'
 import Layout from '../../components/Layout'
-import {showLoading , hideLoading } from '../../redux/alertsSlice'
+import { showLoading, hideLoading } from '../../redux/alertsSlice'
 import axios from 'axios'
-import { useState,useEffect } from 'react'
+//import toast from 'react-hot-toast';
+import { useState, useEffect } from 'react'
+//import { response } from 'express'
 
 function DoctorsList() {
-  const [doctors , setDoctors] = useState([])
+  const [doctors, setDoctors] = useState([])
   const dispatch = useDispatch()
-  const getUsersData =async()=>{
-      try {
-          dispatch(showLoading())
-          const response = await axios.get('./api/admin/get-all-doctors',{
-              headers: {
-                  Authorization:`Bearer ${localStorage.getItem("token")}`,
-              }
-          })
-          dispatch(hideLoading())
-          if(response.data.success){
-              setDoctors(response.data.data)
-          }
-      } catch (error) {
-          dispatch(hideLoading())
-          console.log(error)
+  const getDoctorsData = async () => {
+    try {
+      dispatch(showLoading())
+      const response = await axios.get('/api/admin/get-all-doctors', {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      })
+
+      dispatch(hideLoading())
+      if (response.data.success) {
+        setDoctors(response.data.data)
+        //getDoctorsData();
       }
+    } catch (error) {
+      dispatch(hideLoading())
+      console.log(error)
+    }
+  }
+  const changeDoctorStatus = async (record, status) => {
+    try {
+      dispatch(showLoading())
+      const response = await axios.post('/api/admin/change-doctor-account-status', { doctorId: record._id, userId: record.userId, status: status }, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      })
+
+      dispatch(hideLoading())
+      if (response.data.success) {
+        console.log(response.data.message)
+        getDoctorsData();
+      }
+    } catch (error) {
+      console.log("Error changing doctor account status")
+      dispatch(hideLoading())
+    }
   }
   useEffect(() => {
-    getUsersData();
+    getDoctorsData();
   }, []);
   const columns = [
     {
-        title: 'Name',
-        dataIndex: 'name',
-        render: (text,record) => <span className='normal-text'>{record.firstName}{record.lastName}</span>,
+      title: 'Name',
+      dataIndex: 'name',
+      render: (text, record) => <span className='normal-text'>{record.firstName}{record.lastName}</span>,
     },
     {
       title: 'Phone',
@@ -48,21 +71,22 @@ function DoctorsList() {
       dataIndex: 'status',
     },
     {
-        title: 'Actions',
-        dataIndex: 'actions',
-        render: (text,record)=>(
-            <div className='d-flex'>
-              {record.status === 'pending' && <h1 className='anchor'>Approve</h1>}
-              {record.status === 'Approve' && <h1 className='anchor'>Block</h1>} 
-            </div>
-        )
+      title: 'Actions',
+      dataIndex: 'actions',
+      render: (text, record) => (
+
+        <div className='d-flex'>
+          {record.status === 'pending' && <h1 className='anchor' onClick={() => changeDoctorStatus(record, 'approved')}>Approve</h1>}
+          {record.status === 'approved' && <h1 className='anchor' onClick={() => changeDoctorStatus(record, 'blocked')}>Block</h1>}
+        </div>
+      )
     },
   ]
   return (
     <Layout>
       <h1 className='d-flex'>DoctorsList</h1>
-      <Table columns={columns} dataSource={doctors}/>
-      </Layout>
+      <Table columns={columns} dataSource={doctors} />
+    </Layout>
   )
 }
 
